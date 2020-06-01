a007055d1721dfcac1ac4316c877b6e604aaac71
konrad <sochacki.konrad@gmail.com>
2020-06-01 18:15:52 +0200

    Refactor and add lab 6

--- lab6.py
+++ lab6.py
@@ -48,54 +48,26 @@ assert np.allclose(get_cumulative_rewards([0, 0, 1, 2, 3, 4, 0], gamma=0), [0, 0
 
 
 class Agent(object):
-    def __init__(self, ALPHA, GAMMA=0.99, n_actions=4,
-                 layer1_size=16, layer2_size=16, input_dims=128,
-                 fname='reinforce.h5'):
-        self.gamma = GAMMA
-        self.lr = ALPHA
+    def __init__(self, action_size, policy, predict):
         self.G = 0
-        self.input_dims = input_dims
-        self.fc1_dims = layer1_size
-        self.fc2_dims = layer2_size
-        self.n_actions = n_actions
+        self.gamma = 0.99
+        self.action_size = action_size
+        self.policy = policy
+        self.predict = predict
+        self.action_space = [i for i in range(action_size)]
         self.state_memory = []
         self.action_memory = []
         self.reward_memory = []
-        self.policy, self.predict = self.build_policy_network()
-        self.action_space = [i for i in range(n_actions)]
 
-        self.model_file = fname
-
-    def build_policy_network(self):
-        input = Input(shape=(self.input_dims,))
-        advantages = Input(shape=[1])
-        dense1 = Dense(self.fc1_dims, activation='relu')(input)
-        dense2 = Dense(self.fc2_dims, activation='relu')(dense1)
-        probs = Dense(self.n_actions, activation='softmax')(dense2)
-
-        def custom_loss(y_true, y_pred):
-            out = K.clip(y_pred, 1e-8, 1 - 1e-8)
-            log_lik = y_true * K.log(out)
-
-            return K.sum(-log_lik * advantages)
-
-        policy = Model(input=[input, advantages], output=[probs])
-
-        policy.compile(optimizer=Adam(lr=self.lr), loss=custom_loss)
-
-        predict = Model(input=[input], output=[probs])
-
-        return policy, predict
-
-    def get_action(self, observation):
-        state = observation[np.newaxis, :]
+    def get_action(self, state):
+        state = state[np.newaxis, :]
         probabilities = self.predict.predict(state)[0]
         action = np.random.choice(self.action_space, p=probabilities)
 
         return action
 
-    def store_transition(self, observation, action, reward):
-        self.state_memory.append(observation)
+    def remember(self, state, action, reward):
+        self.state_memory.append(state)
         self.action_memory.append(action)
         self.reward_memory.append(reward)
 
@@ -104,21 +76,9 @@ class Agent(object):
         action_memory = np.array(self.action_memory)
         reward_memory = np.array(self.reward_memory)
 
-        actions = np.zeros([len(action_memory), self.n_actions])
+        actions = np.zeros([len(action_memory), self.action_size])
         actions[np.arange(len(action_memory)), action_memory] = 1
 
-        # G = np.zeros_like(reward_memory)
-        # for t in range(len(reward_memory)):
-        #     G_sum = 0
-        #     discount = 1
-        #     for k in range(t, len(reward_memory)):
-        #         G_sum += reward_memory[k] * discount
-        #         discount *= self.gamma
-        #     G[t] = G_sum
-        # mean = np.mean(G)
-        # std = np.std(G) if np.std(G) > 0 else 1
-        # self.G = (G - mean) / std
-        # self.G = get_cumulative_rewards(reward_memory, self.gamma)
         self.G = np.array(get_cumulative_rewards(reward_memory, self.gamma))
 
         cost = self.policy.train_on_batch([state_memory, self.G], actions)
@@ -135,20 +95,25 @@ state_size = env.observation_space.shape[0]
 action_size = env.action_space.n
 learning_rate = 0.001
 
-#
-# INSERT CODE HERE to build network, in the last layer use softmax activation function
-#
-model = Sequential()
-model.add(Dense(16, input_dim=state_size, activation="relu"))
-model.add(Dense(32, activation="relu"))
-model.add(Dense(16, activation="relu"))
-model.add(Dense(action_size, activation="softmax"))  # wyjście
-model.compile(loss="mean_squared_error",
-              optimizer=Adam(lr=learning_rate))
+input = Input(shape=(state_size,))
+advantages = Input(shape=[1])
+dense1 = Dense(64, activation='relu')(input)
+dense2 = Dense(64, activation='relu')(dense1)
+probs = Dense(action_size, activation='softmax')(dense2)
 
+def custom_loss(y_true, y_pred):
+    out = K.clip(y_pred, 1e-8, 1 - 1e-8)
+    log_lik = y_true * K.log(out)
 
-agent = Agent(ALPHA=0.0005, input_dims=4, GAMMA=0.99,
-                  n_actions=2, layer1_size=64, layer2_size=64)
+    return K.sum(-log_lik * advantages)
+
+policy = Model(input=[input, advantages], output=[probs])
+
+policy.compile(optimizer=Adam(lr=0.0005), loss=custom_loss)
+
+predict = Model(input=[input], output=[probs])
+
+agent = Agent(action_size, policy, predict)
 
 
 def generate_session(t_max=1000):
@@ -166,7 +131,7 @@ def generate_session(t_max=1000):
         new_s, r, done, info = env.step(a)
 
         # record session history to train later
-        agent.store_transition(s, a, r)
+        agent.remember(s, a, r)
 
         reward += r
 
